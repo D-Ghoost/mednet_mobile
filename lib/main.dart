a4ac94a70358495f7492f3d6cea18d51c1b88d91
Nicolás Torres López <nicolas.torreslop@gmail.com>
2023-11-29 16:04:08 -0500

    add:login and validate

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_app_mobiles/views/screens/Foro/foro.dart';
 import 'package:proyecto_app_mobiles/views/screens/Home/Home.dart';
+import 'package:proyecto_app_mobiles/views/screens/Login/Login.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,8 +15,8 @@ class MainApp extends StatelessWidget {
     return  MaterialApp(
       title: 'Foro Flutter',
       routes: {
-        "/":(context) => const Home(),
-        "/foro":(context) =>  const Foro(),
+        "/":(context) => const Login(),
+        "/home":(context) =>  const Home(),
         // "/medicamentos": (context) => const Medicamentos(),
         // "/edicion":(context) => const EdicionPerfil(),
         // "/add-medicamentos": (context) => const MyApp()
